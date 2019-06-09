0b21c0c1f3948339d3d4dbfd0b6613363c624859
fenglai <i_dave@163.com>
2019-06-09 22:09:00 +0800

    init

---
+++ src/main/java/com/dave/pigture/PigtureApplication.java
@@ -0,0 +1,13 @@
+package com.dave.pigture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigtureApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PigtureApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/dave/pigture/PigtureApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dave.pigture;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PigtureApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
